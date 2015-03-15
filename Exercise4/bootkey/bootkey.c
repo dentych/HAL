11e3a5bf549df6614592db9ae51cba9925edd1c2
dentych <dennis@tychsen.me>
2015-03-15 20:08:13 +0100

    Added exercise 4

---
+++ Exercise4/bootkey/bootkey.c
@@ -0,0 +1,88 @@
+#include <linux/gpio.h> 
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <asm/uaccess.h>
+#include <linux/module.h>
+
+#define MAJORNUM 64
+#define MINORNUM 0
+
+#define GPIO_NUM 7
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+static struct cdev my_cdev;
+struct file_operations my_fops;
+
+static int bootkey_init(void) {
+	int errNo;
+	int devno = MKDEV(MAJORNUM, MINORNUM);
+	errNo = gpio_request(GPIO_NUM, "gpio7");
+
+	if (errNo < 0) {
+		printk(KERN_ERR "gpio_request failed: %d", errNo);
+	}
+
+	gpio_direction_input(GPIO_NUM);
+	cdev_init(&my_cdev, &my_fops);
+	
+	errNo = register_chrdev_region(devno, 1, "boot_key");
+	if (errNo < 0) {
+		printk(KERN_ERR "Registration of device number failed! %d\n", errNo);
+	}
+
+	if (cdev_add(&my_cdev, devno, 1) < 0) {
+		printk(KERN_ERR "Cdev_add failed! :(");
+	}
+
+	return 0;
+}
+
+static void bootkey_exit(void) {
+	cdev_del(&my_cdev);
+	unregister_chrdev_region(my_cdev.dev, 1);
+	gpio_free(GPIO_NUM);
+}
+
+int bootkey_open(struct inode *inode, struct file * fp) {
+	int major, minor;
+	major = MAJOR(inode->i_rdev);
+	minor = MINOR(inode->i_rdev);
+	printk("Opening Boot_key device [major], [minor]: %i, %i\n", major, minor);
+
+	return 0;
+}
+
+int bootkey_release(struct inode * inode, struct file * fp) {
+	int major, minor;
+
+	major = MAJOR(inode->i_rdev);
+	minor = MINOR(inode->i_rdev);
+	printk("Closing/releasing Boot_key device [major], [minor]: %i, %i\n", major, minor);
+
+	return 0;
+}
+
+ssize_t bootkey_read(struct file * fp, char __user * buf, size_t count, loff_t * fpos) {
+	int gpioValue = gpio_get_value(GPIO_NUM);
+	char strValue[1];
+	sprintf(strValue, "%d", gpioValue);
+	unsigned long copyFailBytes = copy_to_user(buf, strValue, 1);
+	if (copyFailBytes != 0) {
+		printk(KERN_WARNING "Some bytes could not be copied. Amount: %lu", copyFailBytes);
+	}
+
+	return 1;
+}
+
+struct file_operations my_fops = {
+	.owner = THIS_MODULE,
+	.read = bootkey_read,
+	.open = bootkey_open,
+	.release = bootkey_release,
+};
+
+module_init(bootkey_init);
+module_exit(bootkey_exit);
+
---
+++ Exercise4/sysled4/sysled.c
@@ -0,0 +1,106 @@
+#include <linux/gpio.h> 
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <asm/uaccess.h>
+#include <linux/module.h>
+
+#define MAJORNUM 65
+#define MINORNUM 0
+
+#define GPIO_NUM 163
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+static struct cdev my_cdev;
+struct file_operations my_fops;
+
+static int sysled_init(void) {
+	int errNo;
+	int devno = MKDEV(MAJORNUM, MINORNUM);
+	errNo = gpio_request(GPIO_NUM, "gpio164");
+
+	if (errNo < 0) {
+		printk(KERN_ALERT "gpio_request failed: %d", errNo);
+	}
+
+	gpio_direction_output(GPIO_NUM, 1);
+	cdev_init(&my_cdev, &my_fops);
+	
+	errNo = register_chrdev_region(devno, 1, "sysled4");
+	if (errNo < 0) {
+		printk(KERN_ALERT "Registration of device number failed! %d\n", errNo);
+	}
+
+	if (cdev_add(&my_cdev, devno, 1) < 0) {
+		printk(KERN_ALERT "Cdev_add failed! :(");
+	}
+
+	return 0;
+}
+
+static void sysled_exit(void) {
+	cdev_del(&my_cdev);
+	unregister_chrdev_region(my_cdev.dev, 1);
+	gpio_free(GPIO_NUM);
+}
+
+int sysled_open(struct inode *inode, struct file * fp) {
+	int major, minor;
+	major = MAJOR(inode->i_rdev);
+	minor = MINOR(inode->i_rdev);
+	printk("Opening Sys_led device [major], [minor]: %i, %i\n", major, minor);
+
+	return 0;
+}
+
+int sysled_release(struct inode * inode, struct file * fp) {
+	int major, minor;
+
+	major = MAJOR(inode->i_rdev);
+	minor = MINOR(inode->i_rdev);
+	printk("Closing/releasing Sys_led device [major], [minor]: %i, %i\n", major, minor);
+
+	return 0;
+}
+
+ssize_t sysled_read(struct file * fp, char __user * buf, size_t count, loff_t * fpos) {
+	int gpioValue = gpio_get_value(GPIO_NUM);
+	unsigned long err;
+
+	if (gpioValue == 0) {
+		err = copy_to_user(buf, "0", 2);
+	}
+	else {
+		err = copy_to_user(buf, "1", 2);
+	}
+
+	if (err != 0) {
+		printk(KERN_ALERT "Copy to user gave an error..!\n");
+	}
+
+	return 2;
+}
+
+ssize_t sysled_write(struct file * fp, const char __user * ubuf, size_t count, loff_t * fpos) {
+	int value;
+	if (sscanf(ubuf, "%d", &value) != 1) {
+		printk(KERN_ALERT "Error reading");
+	}
+
+	gpio_set_value(GPIO_NUM, value);
+
+	return count;
+}
+
+struct file_operations my_fops = {
+	.owner = THIS_MODULE,
+	.read = sysled_read,
+	.open = sysled_open,
+	.write = sysled_write,
+	.release = sysled_release,
+};
+
+module_init(sysled_init);
+module_exit(sysled_exit);
+
